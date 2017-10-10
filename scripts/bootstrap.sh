f8098f8525c58218c1e04aa19ceeb03feb6bf723
Denis Arkhipenko <denis-arhipenko@mail.ru>
2017-10-10 18:08:24 +0500

    Update bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,7 +4,7 @@ CONTEST_HOME="/home/ejudge"
 EJUDGE_INSTALL="/opt/scripts/install.sh"
 
 chown -R ejudge:ejudge "${CONTEST_HOME}"
-
+cp -r /opt/scripts/html/* /var/www/ejudge/cgi-bin/
 if [ "$(ls -A $CONTEST_HOME)" ]; then
     echo "Contest directory is not empty. Leaving."
 else
