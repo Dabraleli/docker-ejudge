d906411e2c6a714481190067c57401d4b6acc62d
Denis Arkhipenko <denis-arhipenko@mail.ru>
2017-10-10 18:22:53 +0500

    Update auth.js

--- scripts/html/js/auth.js
+++ scripts/html/js/auth.js
@@ -5,7 +5,15 @@ $(document).ready(function(){
 		if( username != '' && password != ''){
 			$.post("http://" + document.location.host + "/new-client",{ login: username, password: password}, function(data) {
 				alert(data);
+				document.location = document.location.href + "/new-client?contest_id=1&SID=" + getCookie("EJSID");
 			});
 		}
 	});
 });
+
+function getCookie(name) {
+  var matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
