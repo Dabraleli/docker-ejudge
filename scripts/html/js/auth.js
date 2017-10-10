abce9d48965baa00f16ee066cb0400e8793fb7a0
Denis Arkhipenko <denis-arhipenko@mail.ru>
2017-10-10 18:28:31 +0500

    Update auth.js

--- scripts/html/js/auth.js
+++ scripts/html/js/auth.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 		var password = $("#inputPassword").val();
 		if( username != '' && password != ''){
 			$.post("http://" + document.location.host + "/new-client",{ login: username, password: password}, function(data) {
-				alert(data);
+				saveFile(data);
 				document.location = document.location.href + "/new-client?contest_id=1&SID=" + getCookie("EJSID");
 			});
 		}
@@ -16,4 +16,11 @@ function getCookie(name) {
     "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
   ));
   return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function saveFile(data) {
+  var text = data;
+  var filename = "File";
+  var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
+  saveAs(blob, filename+".txt");
 }
